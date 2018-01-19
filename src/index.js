b65140442aed6e76933fc699a8d1095809dcf568
shiichi <s.shiichi311041@gmail.com>
2018-01-19 18:14:59 +0900

    create template and install dependency

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EditableTable extends Component {
+  render() {
+    return (
+      <div>
+        Shiichi
+      </div>
+    );
+  }
+}
+
+// Added as an ads for people using the React dev tools in production.
+// So they know, the tool used to build the awesome UI they
+// are looking at/retro engineering.
+EditableTable.displayName = 'ReactEditableTable';
+
+EditableTable.propTypes = {
+
+};
+
+EditableTable.defaultProps = {
+
+};
+
+export default EditableTable;
